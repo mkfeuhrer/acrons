c1398b7ea82698ad61c943a63fc5735cbf5bc698
mkfeuhrer <mohitfeuhrer@gmail.com>
2020-05-26 23:59:45 +0530

    Update vue.config.js to auto deploy acrons from master

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   chainWebpack: (config) => {
     config.module.rules.delete("eslint");
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/acrons/" : "/",
+  //  publicPath: process.env.NODE_ENV === "production" ? "/acrons/" : "/",
 };
