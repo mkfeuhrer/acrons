f252f5198d925496703d18f339053d481a412816
Mohit Khare <mohitfeuhrer@gmail.com>
2020-08-06 02:48:23 +0530

    Update vue.config.js with right path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  chainWebpack: (config) => {
-    config.module.rules.delete("eslint");
-  },
-  publicPath: process.env.NODE_ENV === "production" ? "/acrons/" : "/",
+  publicPath: "/acrons/",
 };
